06ba80e8ea4c327f699665bbf819d16079a8e02e
Danny Cheun <dcheun@gmail.com>
2021-11-03 13:20:10 -0700

    Add Owners List and updated controller. Closes #16.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -26,7 +26,6 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
     @Override
     public void delete(Pet object) {
         super.delete(object);
-
     }
 
     @Override
